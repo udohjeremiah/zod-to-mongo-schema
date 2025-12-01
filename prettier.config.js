d0fead5c7c22034c5a087a1f1a408fa8624bd057
Udoh Jeremiah <udohjeremiah@icloud.com>
2025-12-01 21:19:48 +0100

    chore: use standard config files
    This uses the latest config files considered standard

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+};
+
+export default config;
